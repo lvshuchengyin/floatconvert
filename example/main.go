913ff6262cecc5e759ee46b170b872e03d97ce58
lscy <lvshuchengyin@gmail.com>
2022-04-15 20:04:36 +0800

    fix example

--- example/main.go
+++ example/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/lvshuchengyin/gofloatconvert"
+	"github.com/lvshuchengyin/gobignumber"
 )
 
 const (
@@ -13,17 +13,45 @@ const (
 func main() {
 	floatString := "123.456789"
 	int64WithPricision := int64(0)
-	int64WithPricision, err := gofloatconvert.FloatStringToInt64(floatString, DecimalPlaces)
+	int64WithPricision, err := gobignumber.FloatStringToInt64(floatString, DecimalPlaces)
 	if err != nil {
 		fmt.Println("FloatStringToInt64 err", err.Error())
 		return
 	}
 	fmt.Printf("FloatStringToInt64, floatString:%v, int64WithPricision:%v\n", floatString, int64WithPricision)
 
-	floatString, err = gofloatconvert.Int64ToFloatString(int64WithPricision, DecimalPlaces)
+	floatString, err = gobignumber.Int64ToFloatString(int64WithPricision, DecimalPlaces)
 	if err != nil {
 		fmt.Println("Int64ToFloatString err", err.Error())
 		return
 	}
 	fmt.Printf("Int64ToFloatString, int64WithPricision:%v, floatString:%v\n", int64WithPricision, floatString)
+
+	{
+		sbn, _ := gobignumber.NewBigNumberFromString("-0.00034567")
+		sbn2, _ := gobignumber.NewBigNumberFromString("-12.345678")
+		sbn3 := sbn.Add(sbn2)
+		fmt.Println("Add", sbn, sbn2, sbn3)
+	}
+
+	{
+		sbn, _ := gobignumber.NewBigNumberFromString("-0.00034567")
+		sbn2, _ := gobignumber.NewBigNumberFromString("-12.345678")
+		sbn3 := sbn.Sub(sbn2)
+		fmt.Println("Sub", sbn, sbn2, sbn3)
+	}
+
+	{
+		sbn, _ := gobignumber.NewBigNumberFromString("-0.00034567")
+		sbn2, _ := gobignumber.NewBigNumberFromString("-12.345678")
+		sbn3 := sbn.Mul(sbn2)
+		fmt.Println("Mul", sbn, sbn2, sbn3)
+	}
+
+	{
+		sbn, _ := gobignumber.NewBigNumberFromString("-0.00034567")
+		sbn2, _ := gobignumber.NewBigNumberFromString("-12.345678")
+		sbn3 := sbn.Quo(sbn2, 10)
+		fmt.Println("Quo", sbn, sbn2, sbn3)
+	}
 }
